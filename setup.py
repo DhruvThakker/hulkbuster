35cd6a7764b04bd413654133f286e24d96430b7d
Remedy441 <dhruvt911997@gmail.com>
2017-06-09 16:36:00 +0530

    Multi Server Hosting Added

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='hulkbuster',
-      version='0.3',
+      version='0.4',
       description='IITBombayX EDX API',
       url='https://github.com/DhruvThakker/hulkbuster',
       author='HulkBuster441',
