891ae5d921759d4943a4e857d4bccfdd58dac103
Remedy441 <dhruvt911997@gmail.com>
2017-06-09 12:16:25 +0530

    Second Successful commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='hulkbuster',
       version='0.1',
       description='The funniest joke in the world',
-      url='http://github.com/storborg/funniest',
+      url='https://github.com/DhruvThakker/hulkbuster',
       author='Flying Circus',
       author_email='flyingcircus@example.com',
       license='MIT',
